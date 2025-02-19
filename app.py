f46fa9e8425612720075c2bdf8dcdba87c79506d
Jan Paulik <jan.paulis@gmail.com>
2025-02-19 10:46:11 +0100

    uprava

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python3
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-	return "Welcome!"
 
-if __name__ == "__main__":
-	app.run()
+
+@app.route('/')
+
+def hello_world():
+
+    return 'Hello, World!'
